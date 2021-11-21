4939ec3674d8cb25c1f719b8317ae91d6ffd18e9
kevinjd0 <91098523+kevinjd0@users.noreply.github.com>
2021-11-22 01:19:07 +0400

    Converted webpage to ejs format Added header.ejs,footer.ejs,index.ejs Installed ejs Changed app.js to use ejs files Created contacts.ejs,register.ejs,login.ejs

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const express = require("express");
 const app = express();
 //send an HTTP response when receiving HTTP GET /
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", { root: __dirname });
+  res.render("index"); //no need for ejs extension
+});
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
 });
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
